6a2deb26e20ed4f2045f2b4293545ce229731908
lipemat <mat@matlipe.com>
2022-07-15 13:52:58 -0400

    Fix partial payments for Venmo
    JS for the wp-invoice plugin requires a couple of hidden inputs
    to be available when working with partial payments.
    
    Does not for this gateway, but prevents an alert from displaying.

--- lib/gateways/templates/wpi_venmo-frontend.tpl.php
+++ lib/gateways/templates/wpi_venmo-frontend.tpl.php
@@ -4,6 +4,18 @@ global $invoice, $wpi_settings;
 
 ?>
 <input type="hidden" id="wpi_form_invoice_id" name="invoice_id" value="<?= es( $invoice['invoice_id'] ) ?>" />
+<input
+	type="hidden"
+	id="payment_amount"
+	name="amount"
+	value="<?= esc_attr( number_format( (float) $invoice['net'], 2, '.', '' ) ) ?>">
+<input
+	type="hidden"
+	id="my_amount"
+	name="amount"
+	value="<?= esc_attr( number_format( (float) $invoice['net'], 2, '.', '' ) ) ?>">
+
+
 <p>&nbsp;</p>
 <h3 style="text-align: center;">
 	Memo: Invoice #<?= es( $invoice['ID'] ) ?>
@@ -11,5 +23,5 @@ global $invoice, $wpi_settings;
 <p style="text-align: center;">
 	<img
 		style="width: 30%; height: auto;"
-		src="<?= es( $wpi_settings['billing']['wpi_venmo']['settings']['qrcode']['value'] ) ?>" />
+		src="<?= esc_attr( $wpi_settings['billing']['wpi_venmo']['settings']['qrcode']['value'] ) ?>" />
 </p>
